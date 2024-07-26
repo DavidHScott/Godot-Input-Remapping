ad9e1b3eef5d889515a670c8ea78d43af4c73e5d
David Scott <david@mysticawesome.com>
2024-07-26 01:22:11 -0400

    Input button now listens for modifiers such as shift or ctrl

--- scripts/input_remapping/InputButton.gd
+++ scripts/input_remapping/InputButton.gd
@@ -17,13 +17,28 @@ func _toggled(button_pressed):
 
 
 func _unhandled_input(event):
-	if event.is_pressed():
-		button_pressed = false
-		grab_focus()
-		update_text(event.as_text())
-		set_process_unhandled_input(false)
-	
-	print(event)
+	if event is InputEventKey:
+		
+		update_text(OS.get_keycode_string(event.get_key_label_with_modifiers()))
+		
+		
+		if event.is_pressed():
+			
+			# If the key is a normal key, stop listening. If it is a modifier, keep listening
+			if(event.get_keycode() != KEY_SHIFT and event.get_keycode() != KEY_CTRL and event.get_keycode() != KEY_ALT):
+				button_pressed = false
+				grab_focus()
+				set_process_unhandled_input(false)
+				
+				return
+		elif event.is_released():
+			# This will always mean that a modifier was released, because if a normal key was pressed at all, then it would be returned above
+			pass
+		
+
+
+func confirm_new_input():
+	pass
 
 
 func update_text(name:String):
